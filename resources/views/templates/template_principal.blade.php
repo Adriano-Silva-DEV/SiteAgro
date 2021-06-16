407e788f46c33667736ed79d725c9f51f7ef4846
ADRIANO SILVA <adrianosilvaxp@gmail.com>
2021-06-16 19:31:02 -0300

    novaInterface

--- resources/views/templates/template_principal.blade.php
+++ resources/views/templates/template_principal.blade.php
@@ -12,15 +12,21 @@
 </head>
 
 <body>
-    <div class="container-fluid m-0 ">
+    <div class="container-fluid m-0 headColor shadow-sm  ">
         <header>
 
 
-            {{-- <div class="header shadow-sm p-3 mb-5 bg-light rounded"> --}}
+            <nav class="navbar    mb-5 bg-light8 rounded navbar-expand-lg navbar-dark  ">
+                <div class="container-fluid ">
+                    <a class="navbar-brand " href="#">
+                        <img src="img/logosansal.png" class="logosansal" alt="SANSAL" >
+                                              
+                    </a>
+                    <div > 
+                        <span class="sansal1" >SANSAL</span> <br>
+                        <span class="sansal2" > Nutrientes Animais  </span> 
+                    </div>
 
-            <nav class="navbar shadow-sm  p-3 mb-5 bg-light rounded navbar-expand-lg navbar-light ">
-                <div class="container-fluid">
-                    <a class="navbar-brand px-5" href="#">SANSAL</a>
                     <button class="navbar-toggler" type="button" data-bs-toggle="collapse"
                         data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false"
                         aria-label="Toggle navigation">
@@ -28,7 +34,7 @@
                     </button>
                     <div class="collapse navbar-collapse justify-content-end mx-5" id="navbarNavAltMarkup">
                         <div class="navbar-nav  ">
-                            <a class="nav-link  active" aria-current="page" href="#">Home</a>
+                            <a class="nav-link  active" aria-current="page" href="#">Inicio</a>
                             <a class="nav-link " href="#quem-somos">Quem somos</a>
                             <a class="nav-link " href="#produtos">Produtos</a>
                             <a class="nav-link " href="#contato">Contato</a>
@@ -37,7 +43,7 @@
                 </div>
             </nav>
 
-            {{-- </div> --}}
+
 
         </header>
     </div>
@@ -47,33 +53,51 @@
 
 
     <div class="footer">
-
+        <div class="rodape"> </div>
         <div class="container-fluid m-0 body text-center">
 
-            <div class="row justify-center">
-                <div class="col-12  col-sm-6 card-rodape">
-                   <h5>Contato:</h5>
-                   
-                   TEL: (65) 3361-1414 <br>
-                   Email: financeiro@sansal.com.br
-                </div>
-                <div class="col-12 col-sm-6 p-5">
 
-                  
-                    <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2291.138282881882!2d-57.18382501555524!3d-15.035946407093691!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0x710db97a14b1c261!2sSansal%20Nutrientes%20Animais!5e0!3m2!1spt-BR!2sbr!4v1623358775008!5m2!1spt-BR!2sbr"
-                     class="mapa"  style="border:0;" allowfullscreen="" loading="lazy"></iframe>
-                </div>
-                <div class="col-12  ">
+
+            <div class="row justify-center">
+                <div class="col-12 col-sm-6 ">
+
+                    <div class="row justify-center">
+                      
+                        <div class=" col-sm-6  card-rodape">
+                            <h5>Contato:</h5>
+                            TEL: (65) 3361-1414 <br>
+                            Email: financeiro@sansal.com.br
+                        </div>
                    
+                        <div class="col-sm-6   card-rodape">
+                            <h5>Contato:</h5>
+                            TEL: (65) 3361-1414 <br>
+                            Email: financeiro@sansal.com.br
+                        </div>
+
+                   
+                    </div>
+
+                                        
+                </div>
+                    <div class="col-12 col-sm-6 p-5">
+
+
+                        <iframe
+                            src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2291.138282881882!2d-57.18382501555524!3d-15.035946407093691!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0x710db97a14b1c261!2sSansal%20Nutrientes%20Animais!5e0!3m2!1spt-BR!2sbr!4v1623358775008!5m2!1spt-BR!2sbr"
+                            class="mapa shadow" style="border:0;" allowfullscreen="" loading="lazy"></iframe>
+                    </div>
+                    <div class="col-12  ">
+
+                    </div>
                 </div>
             </div>
+
         </div>
-    
-    </div>
 
-    <div class=" text-center"> Sansal @ Todos os direitos reservados 2021 </div>
+        <div class=" text-center"> Sansal @ Todos os direitos reservados 2021 </div>
 
-    <script src="{{ asset('js/bootstrap/bootstrap.min.js') }}"></script>
+        <script src="{{ asset('js/bootstrap/bootstrap.min.js') }}"></script>
 </body>
 
 </html>
